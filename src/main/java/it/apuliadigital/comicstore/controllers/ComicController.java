c7161bf62321b3ab1f6b3ac55c329dc8ccbca158
giulia li bergolis <glibergolis@studenti.apuliadigital.it>
2026-05-27 16:47:40 +0200

    ciao

--- src/main/java/it/apuliadigital/comicstore/controllers/ComicController.java
+++
@@ -1,9 +0,0 @@
-package it.apuliadigital.comicstore.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/comics")
-public class ComicController {
-}
--- src/main/java/it/apuliadigital/comicstore/repositories/ComicRepository.java
+++ src/main/java/it/apuliadigital/comicstore/repositories/ComicRepository.java
@@ -1,7 +1,18 @@
 package it.apuliadigital.comicstore.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import it.apuliadigital.comicstore.models.Comic;
+
 @Repository
-public interface ComicRepository {
+public interface ComicRepository extends JpaRepository<Comic, Long> {
+
+    boolean existsByTitleAndAuthorAndGenreAndPrice(String title, String author, String genre, double price);
+
+    Optional<Comic> findByTitleIgnoreCase(String title);
+
+    
 }
